b0a8b11a05c691f95605803024d21bb819dcd548
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-04-08 16:52:47 +0530

    checkpoint 1

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
 import { generate } from "./utils";
+import { getAllFiles } from "./file";
+import path from "path";
 
 const app = express();
 app.use(cors());
@@ -14,6 +16,8 @@ app.post("/deploy", async (req, res)=>{
     const repoUrl = req.body.repoUrl
     const id = generate()
     await simpleGit().clone(repoUrl, `output/${id}`)
+
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
     console.log(repoUrl);
     res.json({
         id: id
